55876110be5ab0dc996d85a825a8823654ac40d6
Pattarakrit Rattanukul <jdanceze@Pattarakrits-MacBook-Pro.local>
2022-02-25 12:21:20 +0700

    merge sth

--- Web/app.js
+++ Web/app.js
@@ -219,9 +219,9 @@ app.get("/testingtool", (req, res) => {
 app.get("/result", (req, res) => {
   // if (notauth(req, res)) return;
   let user = getUser(req);
-  //fs.readFile('../example/output3.json', 'utf8', (err, data) => {
+  fs.readFile('../example/output5.json', 'utf8', (err, data) => {
 
-  fs.readFile("../output/output.json", "utf8", (err, data) => {
+  //fs.readFile("../output/output.json", "utf8", (err, data) => {
     if (err) {
       return console.log("File read failed:", err);
     }
